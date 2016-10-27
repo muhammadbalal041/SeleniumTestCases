15b1659bbc442350802e8a8262c67269876f21b7
muhammadbalal041 <muhammadbalal041@gmail.com>
2016-10-27 17:44:47 +0500

    Update TestRunnerMain.java

--- src/main/java/example/TestRunnerMain.java
+++ src/main/java/example/TestRunnerMain.java
@@ -20,28 +20,29 @@ import com.sun.jna.platform.FileUtils;
 public class TestRunnerMain {
 
  	public static void main(String[] args) {
-		TestListenerAdapter tla = new TestListenerAdapter();
-		TestNG testng = new TestNG();
-		testng.setTestClasses(new Class[] { PlaceAnOrder.class });
-		testng.addListener(tla);
-		testng.run();
 		
-// 		String Xport = System.getProperty(
-//                 "lmportal.xvfb.id", ":1");
-//         final File firefoxPath = new File(System.getProperty(
-//                 "lmportal.deploy.firefox.path", "/usr/bin/firefox"));
-//         FirefoxBinary firefoxBinary = new FirefoxBinary(firefoxPath);
-//         firefoxBinary.setEnvironmentProperty("DISPLAY", Xport);
+// 		TestListenerAdapter tla = new TestListenerAdapter();
+// 		TestNG testng = new TestNG();
+// 		testng.setTestClasses(new Class[] { PlaceAnOrder.class });
+// 		testng.addListener(tla);
+// 		testng.run();
+		
+		String Xport = System.getProperty(
+                "lmportal.xvfb.id", ":1");
+        final File firefoxPath = new File(System.getProperty(
+                "lmportal.deploy.firefox.path", "/usr/bin/firefox"));
+        FirefoxBinary firefoxBinary = new FirefoxBinary(firefoxPath);
+        firefoxBinary.setEnvironmentProperty("DISPLAY", Xport);
 
 //         // Start Firefox driver
-//         WebDriver driver = new FirefoxDriver(firefoxBinary, null);
-//         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-//         driver.get("http://google.com/");
-//         System.out.println("Page title is: " + driver.getTitle());
-//         // Take snapshot of browser
-//         //File srcFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-//         //FileUtils.copyFile(srcFile, new File("ffsnapshot.png"));
-//         driver.quit();
+        WebDriver driver = new FirefoxDriver(firefoxBinary, null);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("http://google.com/");
+        System.out.println("Page title is: " + driver.getTitle());
+        // Take snapshot of browser
+        //File srcFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        //FileUtils.copyFile(srcFile, new File("ffsnapshot.png"));
+        driver.quit();
 // 	}
 
 		
