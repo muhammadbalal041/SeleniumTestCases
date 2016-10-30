2121283512e81010575448b3291b1545eea06241
muhammadbalal041 <muhammadbalal041@gmail.com>
2016-10-30 23:39:29 +0500

    Add files via upload

---
+++ src/main/java/example/xvfb2.java
@@ -0,0 +1,31 @@
+package example;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class xvfb2 {
+  @Test
+  public void f() {
+	  String Xport = System.getProperty(
+              "lmportal.xvfb.id", ":10");
+      final File firefoxPath = new File(System.getProperty(
+              "lmportal.deploy.firefox.path", "/usr/bin/firefox"));
+      FirefoxBinary firefoxBinary = new FirefoxBinary(firefoxPath);
+      firefoxBinary.setEnvironmentProperty("DISPLAY", Xport);
+
+      // Start Firefox driver
+      WebDriver driver = new FirefoxDriver(firefoxBinary, null);
+      driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+      driver.get("http://google.com/");
+      System.out.println("Page title is: " + driver.getTitle());
+      //Take snapshot of browser
+      //File srcFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+      //FileUtils.copyFile(srcFile, new File("ffsnapshot.png"));
+      driver.quit();
+  }
+}
