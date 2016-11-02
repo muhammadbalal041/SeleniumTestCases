17a41d602b646e79ab1a0921080cbe26b5ccaffc
muhammadbalal041 <muhammadbalal041@gmail.com>
2016-11-02 20:18:45 +0500

    Update xvfb2.java

--- src/main/java/example/xvfb2.java
+++ src/main/java/example/xvfb2.java
@@ -122,6 +122,9 @@ public class xvfb2 {
 				//*[@id="swatch11"]
 				//*[@id="swatch14"]
 				//*[@id="swatch11"]
+				WebDriverWait wait = new WebDriverWait(driver, 100);
+				wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='swatch11']")));
+
 				driver.findElement(By.xpath("//*[@id='swatch11']")).click();
 				System.out.println("swatch size is clicked");
 				
